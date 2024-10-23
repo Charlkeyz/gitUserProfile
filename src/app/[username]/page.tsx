03c510943895f9d7d3b3101bdeb6264c3d8f411b
Charles ugochukwu <Charlkeyz2017@gmail.com>
2024-10-23 03:07:13 +0100

    commit code

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -4,10 +4,8 @@ import { useGithubContext } from '@/GithubContextStore/ContextStore'
 import { useParams } from 'next/navigation'
 // import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
-type UserProfilePageProps = {
-    className : string
-}
-export default function UserProfilePage({className}: UserProfilePageProps) {
+
+export default function UserProfilePage() {
     const {fetchGithubProfile} = useGithubContext()
 
     const {username} = useParams()
